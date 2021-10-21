ae5688143139a39e4c398966be7f7853c3cbaf94
PeterSchlenker <91510378+PeterSchlenker@users.noreply.github.com>
2021-10-21 16:49:02 -0500

    Moving Platforms!
    I implemented moving platforms. I also made a lot of additions and changes to the playerController class so that players stick to and interact with moving platforms correctly.

---
+++ Moving Platforms/Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+
+    // VARIABLES
+    public static GameManager instance;
+
+    public static string[] powerUpOptions = { "slow", "skip", "speed",
+                                        "pause", "rewind" };
+
+    public static string currentPowerUp;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (GameManager.instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // throw new NotImplementedException();
+    }
+
+    #region powerup
+    public void CollectPowerUp()
+    {
+        currentPowerUp = powerUpOptions[UnityEngine.Random.Range(0, powerUpOptions.Length)];
+        
+        Debug.Log("currentPowerUp: " + currentPowerUp);
+
+    }
+    #endregion powerup
+
+}
